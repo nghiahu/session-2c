96aafb1e2a15bff15234faabebd7ddb5bdc8bb2e
nghiahu <147359597+nghiahu@users.noreply.github.com>
2023-11-07 22:30:45 +0700

    Add files via upload

---
+++ session 1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	float canhhinhvuong = 5, canhdaihinhchunhat = 4, cachnganhinhchunhat = 3, bankinhduongtron = 6;
+	float Pi = 3.14;
+	float chuvihinhvuong = 4 * 5, dientinhhinhvuong = 5 * 5;
+	float chuvihinhchunhat = (4 + 3) * 2, dientinhhinhchunhat = 4 * 3;
+	float chuvihinhtron = (6 * 2) * 3.14, dientinhhinhtron = (6 * 6) * 3.14;
+	
+}
